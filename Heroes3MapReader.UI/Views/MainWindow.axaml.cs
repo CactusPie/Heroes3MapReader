81c15da933e04d55e3f5be927557e5e4f0607dc9
CactusPie <mateuszdudek990@gmail.com>
2026-02-02 20:46:02 +0100

    Made the map description, map name and file path selectable

--- Heroes3MapReader.UI/Views/MainWindow.axaml.cs
+++ Heroes3MapReader.UI/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Heroes3MapReader.UI.Views;
 
@@ -7,5 +8,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        base.OnPointerPressed(e);
+        FocusManager?.ClearFocus();
     }
 }
